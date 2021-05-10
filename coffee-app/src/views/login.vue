6db250e9d29833dcaba09fc205d6247175ce7eb2
HarunaYamaguchi <haruna.yamaguchi@rakus-partners.co.jp>
2021-05-10 14:29:35 +0900

    storeへのログイン機能追加しました

---
+++ coffee-app/src/views/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="login">ログイン</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default ({
+  methods:{
+    ...mapActions(["login"])
+  },
+})
+</script>
