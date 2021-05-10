106d4bdd7a24b3501bfafb0f1ff46d573b0fbeaa
IHprogram <ihm20202020@gmail.com>
2021-05-10 14:57:25 +0900

    Firebaseのインストール

--- coffee-app/src/store/index.js
+++ coffee-app/src/store/index.js
@@ -1,33 +1,32 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import firebase from 'firebase'
+import Vue from "vue";
+import Vuex from "vuex";
+import firebase from "firebase";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
     login_user: null,
   },
   mutations: {
-    setLoginUser(state,user){
-      state.login_user = user
+    setLoginUser(state, user) {
+      state.login_user = user;
     },
-    deleteLoginUser(state){
-      state.login_user　=　null
-    }
+    deleteLoginUser(state) {
+      state.login_user = null;
+    },
   },
   actions: {
-    login(){
-      const google_auth_provider = new firebase.auth.GoogleAuthProvider()
-      firebase.auth().signInWithRedirect(google_auth_provider)
+    login() {
+      const google_auth_provider = new firebase.auth.GoogleAuthProvider();
+      firebase.auth().signInWithRedirect(google_auth_provider);
     },
-    logout(){
-      firebase.auth().signOut()
+    logout() {
+      firebase.auth().signOut();
     },
-    deleteLoginUser({commit}){
-      commit('deleteLoginUser')
+    deleteLoginUser({ commit }) {
+      commit("deleteLoginUser");
     },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
