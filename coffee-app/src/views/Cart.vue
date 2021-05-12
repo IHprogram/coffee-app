b8769ddecc512113bb2b1ab9ccdd1d606b1e081e
IHprogram <ihm20202020@gmail.com>
2021-05-12 10:21:49 +0900

    カートの中身表示画面のリンクを修正

--- coffee-app/src/views/Cart.vue
+++ coffee-app/src/views/Cart.vue
@@ -3,30 +3,37 @@
     <div class="container">
       <div class="row">
         <div
-          class="table-responsive col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-10 col-xs-12">
+          class="table-responsive col-lg-offset-1 col-lg-10 col-md-offset-1 col-md-10 col-sm-10 col-xs-12"
+        >
           <h3 class="text-center mt-4">ショッピングカート</h3>
           <table class="table table-striped item-list-table mt-4">
             <tbody>
               <tr>
                 <th>
-                  <div class="text-center">
-                    商品名
-                  </div>
+                  <div class="text-center">商品名</div>
                 </th>
                 <th>
-                  <div class="text-center">
-                    価格(税込)
-                  </div>
+                  <div class="text-center">価格(税込)</div>
                 </th>
                 <th>
-                  <div class="text-center">
-                    ー
-                  </div>
+                  <div class="text-center">ー</div>
                 </th>
               </tr>
-              <tr v-for="(order,index) in orders" :key=order.id class="text-center">
-             <td><router-link :to="{name: 'item_detail', params:{item_id: order.id}}">{{order.name}}</router-link></td>
-                <td>{{order.price}}</td>
+              <tr
+                v-for="(order, index) in orders"
+                :key="order.id"
+                class="text-center"
+              >
+                <td>
+                  <router-link
+                    :to="{
+                      name: 'item_detail',
+                      params: { item_id: order.itemId },
+                    }"
+                    >{{ order.name }}</router-link
+                  >
+                </td>
+                <td>{{ order.price }}</td>
                 <td><button @click="deleteOrder(index)">削除</button></td>
               </tr>
               <tr v-if="orders.length === 0">
@@ -36,12 +43,14 @@
           </table>
         </div>
       </div>
-					
+
       <div class="row">
         <div class="col-xs-offset-2 col-xs-8">
           <div class="form-group text-center" v-if="orders.length != 0">
-            <span id="total-price">ご注文金額合計：{{totalPrice}}円(税込)</span><br>
-            <span id="total-price">内消費税：{{taxPrice}}円(税込)</span>
+            <span id="total-price"
+              >ご注文金額合計：{{ totalPrice }}円(税込)</span
+            ><br />
+            <span id="total-price">内消費税：{{ taxPrice }}円(税込)</span>
           </div>
         </div>
       </div>
@@ -51,11 +60,18 @@
           <div class="form-group">
             <div class="form-group text-center" v-if="orders.length != 0">
               <form action="order_confirm.html">
-                <input class="form-control btn btn-warning btn-block mt-4"
-                  type="submit" value="注文に進む">
+                <input
+                  class="form-control btn btn-warning btn-block mt-4"
+                  type="submit"
+                  value="注文に進む"
+                />
               </form>
             </div>
-            <p class="text-center mt-4"><router-link :to="{name: 'Home'}">トップページに戻る</router-link></p>
+            <p class="text-center mt-4">
+              <router-link :to="{ name: 'Home' }"
+                >トップページに戻る</router-link
+              >
+            </p>
           </div>
         </div>
       </div>
@@ -64,28 +80,27 @@
 </template>
 
 <script>
-export default{
-  name:"Cart",
-    data(){
-    return{
+export default {
+  name: "Cart",
+  data() {
+    return {
       orders: this.$store.state.orders,
     };
- },
-  methods:{
-    deleteOrder(index){
-      if(confirm("カートから削除しますか？"))
-      this.orders.splice(index, 1)
+  },
+  methods: {
+    deleteOrder(index) {
+      if (confirm("カートから削除しますか？")) this.orders.splice(index, 1);
     },
   },
   computed: {
-    totalPrice(){
-      const total = this.orders.reduce((a, b) => a + b.price, 0)
+    totalPrice() {
+      const total = this.orders.reduce((a, b) => a + b.price, 0);
       return total;
     },
-    taxPrice(){
+    taxPrice() {
       const tax = this.totalPrice * 0.1;
       return tax;
-    }
-  }
+    },
+  },
 };
 </script>
