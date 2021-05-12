357a78e1db71c4c7c51b295bed504076f79138b6
Chigusa-Sato <chigusa.sato@rakus-partners.co.jp>
2021-05-12 09:39:40 +0900

    合計金額と注文ボタンの表示・非表示

--- coffee-app/src/views/Cart.vue
+++ coffee-app/src/views/Cart.vue
@@ -26,6 +26,9 @@
 							<td></td>
 							<td><button>削除</button></td>
 						</tr>
+						<tr v-if="orders.length===0">
+							<td>カートに商品がありません</td>
+						</tr>
 					
 					</tbody>
 				</table>
@@ -33,14 +36,13 @@
 		</div>
 
 		<div class="row">
-			<div class="col-xs-offset-2 col-xs-8">
+			<div class="col-xs-offset-2 col-xs-8" v-if="orders.length!=0">
 				<div class="form-group text-center">
-					<span id="total-price" >消費税：円</span><br>
-					<span id="total-price">ご注文金額合計：円 (税込)</span>
+					<span id="total-price" >消費税：{{taxOnlyPrice}}円</span><br>
+					<span id="total-price">ご注文金額合計：{{taxInclutedPrice}}円 (税込)</span>
 				</div>
 			</div>
 		</div>
-
 		<div class="row">
 			<div class="col-xs-offset-5 col-xs-3">
 				<div class="form-group">
@@ -62,69 +64,14 @@ export default{
 	name:"Cart",
     data(){
     return{
-        items:[
-            {
-        id:1,
-        name: "Gorgeous4サンド",
-        description: "人気の定番具材「ハム」と「チキン」をセットにした食べごたえ抜群のサンドイッチです。",
-        price: 480,
-        imagePath: "../assets/img/1.jpg"
-      },
+        items:[],
+orders:[
       {
-        id:2,
-        name: "エスプレッソフラペチーノ",
-        description: "ひと口目に感じるエスプレッソは「リストレット」という方法で抽出した力強い香りと優しい苦味を、ふた口目は全体を混ぜて、こだわりのクリームから来るアフォガートのような味わいをお楽しみください。リフレッシュしたい時や、ほっとひと息つきたい時にも、何度でも飲みたくなるフラペチーノ®です.",
-        price: 530,
-        imagePath: "../assets/img/2.jpg"
-      },
-      {
-        id:3,
-        name: "Specialキャラメルドーナッツ",
-        description: "ドーナツ生地の中に、なめらかで風味豊かなキャラメルフィリングを入れ、仕上げにキャラメルのビター感と香ばしさが楽しめるキャラメルコーティングをかけました。",
-        price: 250,
-        imagePath: "../assets/img/3.jpg"
-      },
-      {
-        id:4,
-        name: 'チョコクッキー',
-        description: 'ソフトな食感のクッキー生地には、小麦の香ばしさが感じられるよう全粒粉を入れ、砂糖の一部にはブラウンシュガーを使い、コクのある甘さをプラスしています。風味豊かなスターバックスオリジナルのチョコレートチャンクがごろごろ入っていて、どこを食べてもチョコレートの味わいを存分に楽しめます。ショートサイズのマグカップの上に乗せられるくらいのサイズは、コーヒーと一緒に楽しむのにもぴったりです。',
-        price: 190,
-        imagePath: '../assets/img/4.jpg'
-      },
-      {
-        id:5,
-        name: 'カフェモカ',
-        description: 'エスプレッソにほろ苦いチョコレートシロップとミルクを加え、ホットビバレッジにはホイップクリームをトッピング。ちょっとした贅沢を楽しみたい方におすすめです。',
-        price: 400,
-        imagePath: '../assets/img/5.jpg'
-      },
-      {
-        id:6,
-        name: 'カフェラテ',
-        description: '最も人気のあるエスプレッソ ビバレッジ。リッチなエスプレッソにスチームミルクを注ぎ、フォームミルクを丁寧にトッピングしました。ミルキーな味わいで気持ちを温かくしてくれます。',
-        price: 340,
-        imagePath: '../assets/img/6.jpg'
-      },
-      {
-        id:7,
-        name: 'カプチーノ',
-        description: 'リッチなエスプレッソに一気にミルクを注ぐことで、一口飲んだときからコーヒー感が感じられるビバレッジです。ベルベットのようにきめ細かいフォームミルクをお楽しみください。',
-        price: 340,
-        imagePath: '../assets/img/7.jpg'
-      },
-      {
-        id:8,
-        name: 'キャラメルマキアート',
-        description: 'バニラシロップとスチームミルクのコンビネーションになめらかなフォームミルクをたっぷりのせ、その上からエスプレッソを注いでアクセントを付けました。仕上げにオリジナルキャラメルソースをトッピングしています。',
-        price: 390,
-        imagePath: '../assets/img/8.jpg'
-      },
-      {
-        id:9,
-        name: 'キャラメルフラペチーノ',
-        description: 'コーヒー、ミルク、キャラメルシロップを氷とブレンドした、多くのお客様に愛されているフローズン ビバレッジです。トッピングしたホイップクリームとキャラメルソースと混ぜながら、豊かでほんのり香ばしいキャラメルの風味をお楽しみください。',
+        id:10,
+        name: 'バニラ クリーム フラペチーノ',
+        description: 'ミルクとバニラシロップを氷とブレンドし、ホイップクリームをトッピングした、クリーミーな味わいのフローズン ビバレッジ。真っ白な見た目も爽やかです。ミルクとバニラシロップというシンプルな組み合わせはいろいろなカスタマイズとも好相性。',
         price: 490,
-        imagePath: '../assets/img/9.jpg'
+        imagePath: '../assets/img/10.jpg'
       },
       {
         id:10,
@@ -133,52 +80,27 @@ export default{
         price: 490,
         imagePath: '../assets/img/10.jpg'
       },
-     
-        ],
-orders:[
-            {
-        id:1,
-        name: "Gorgeous4サンド",
-        description: "人気の定番具材「ハム」と「チキン」をセットにした食べごたえ抜群のサンドイッチです。",
-        price: 480,
-        imagePath: "../assets/img/1.jpg"
-      },
-      {
-        id:2,
-        name: "エスプレッソフラペチーノ",
-        description: "ひと口目に感じるエスプレッソは「リストレット」という方法で抽出した力強い香りと優しい苦味を、ふた口目は全体を混ぜて、こだわりのクリームから来るアフォガートのような味わいをお楽しみください。リフレッシュしたい時や、ほっとひと息つきたい時にも、何度でも飲みたくなるフラペチーノ®です.",
-        price: 530,
-        imagePath: "../assets/img/2.jpg"
-      },
-      {
-        id:3,
-        name: "Specialキャラメルドーナッツ",
-        description: "ドーナツ生地の中に、なめらかで風味豊かなキャラメルフィリングを入れ、仕上げにキャラメルのビター感と香ばしさが楽しめるキャラメルコーティングをかけました。",
-        price: 250,
-        imagePath: "../assets/img/3.jpg"
-      },
-      {
-        id:4,
-        name: 'チョコクッキー',
-        description: 'ソフトな食感のクッキー生地には、小麦の香ばしさが感じられるよう全粒粉を入れ、砂糖の一部にはブラウンシュガーを使い、コクのある甘さをプラスしています。風味豊かなスターバックスオリジナルのチョコレートチャンクがごろごろ入っていて、どこを食べてもチョコレートの味わいを存分に楽しめます。ショートサイズのマグカップの上に乗せられるくらいのサイズは、コーヒーと一緒に楽しむのにもぴったりです。',
-        price: 190,
-        imagePath: '../assets/img/4.jpg'
-      },
-      {
-        id:5,
-        name: 'カフェモカ',
-        description: 'エスプレッソにほろ苦いチョコレートシロップとミルクを加え、ホットビバレッジにはホイップクリームをトッピング。ちょっとした贅沢を楽しみたい方におすすめです。',
-        price: 400,
-        imagePath: '../assets/img/5.jpg'
+        {
+        id:10,
+        name: 'バニラ クリーム フラペチーノ',
+        description: 'ミルクとバニラシロップを氷とブレンドし、ホイップクリームをトッピングした、クリーミーな味わいのフローズン ビバレッジ。真っ白な見た目も爽やかです。ミルクとバニラシロップというシンプルな組み合わせはいろいろなカスタマイズとも好相性。',
+        price: 490,
+        imagePath: '../assets/img/10.jpg'
       },
 ],
-    
+totalPrice: 0,
+taxInclutedPrice: 0,
+taxOnlyPrice: 0
     };
  },
-    methods:{
-      delete(){},
-            },
-	computed:{
+ mounted(){
+for(let i = 0; i<this.orders.length;i++){
+this.totalPrice += this.orders[i].price
+}
+this.taxOnlyPrice = Math.round(this.totalPrice*0.1)
+this.taxInclutedPrice=Math.round(this.totalPrice*1.1)
+ },
+computed:{
     order(){
 		return this.$store.state.orders
 	}
